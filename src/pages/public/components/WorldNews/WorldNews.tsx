4ee46cdbb8cb4a633f8220633eae54e5200f5879
jerson100 <juamkoo@gmail.com>
2022-11-06 14:48:33 -0500

    Reorganizando los item flex en versión móvil y desktop

--- src/pages/public/components/WorldNews/WorldNews.tsx
+++ src/pages/public/components/WorldNews/WorldNews.tsx
@@ -5,13 +5,13 @@ import WorldNewsRight from "./WorldNewsRight";
 const WorldNews = () => {
   return (
     <div className="flex flex-col lg:flex-row gap-8">
-      <div className="lg:flex-shrink-1 lg:basis-2/12">
+      <div className="lg:flex-shrink-1 lg:basis-2/12 order-3 lg:order-1">
         <WorldNewsLeft />
       </div>
-      <div className="lg:flex-shrink-1 lg:basis-6/12">
+      <div className="lg:flex-shrink-1 lg:basis-6/12 order-1 lg:order-2">
         <WorldNewsCenter />
       </div>
-      <div className="lg:flex-shrink-1 lg:basis-4/12">
+      <div className="lg:flex-shrink-1 lg:basis-4/12 order-2 lg:order-3">
         <WorldNewsRight />
       </div>
     </div>
