3ca7b4d9733c219a7ec406c29d0a2c5c7479ca07
jerson100 <juamkoo@gmail.com>
2022-11-06 19:17:27 -0500

    Agregamos nuevos svg (social networks)

--- src/components/commons/IconButtton/IconButtonProps.ts
+++ src/components/commons/IconButtton/IconButtonProps.ts
@@ -1,7 +1,15 @@
 import { ButtonProps } from "../Button/buttonProps";
 
 interface IconButtonProps extends ButtonProps {
-  svg: "left" | "right" | "menu";
+  svg:
+    | "left"
+    | "right"
+    | "menu"
+    | "facebook"
+    | "twitter"
+    | "instagram"
+    | "youtube"
+    | "linkedin";
 }
 
 export type { IconButtonProps };
