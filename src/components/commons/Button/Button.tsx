0e1c9ca640c75d55a3800cd62dd27d6150e4ac23
jerson100 <juamkoo@gmail.com>
2022-11-15 12:27:45 -0500

    Separamos los objetos de estilos del botón en un archivos de constantes

--- src/components/commons/Button/Button.tsx
+++ src/components/commons/Button/Button.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import classnames from "classnames";
 import { ButtonProps } from "./buttonProps";
-import { classes } from "./button.style";
+import {
+  BUTTON_DISABLED_STYLE,
+  BUTTON_FULL_WIDTH_STYLE,
+  BUTTON_ROOT_STYLE,
+  BUTTON_SIZE_STYLE,
+  BUTTON_VARIANTS_STYLE,
+} from "../../../consts/button.style";
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
@@ -19,14 +25,14 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     ref
   ) => {
     const classN = classnames(
-      classes.base,
-      classes.sizes[size],
+      BUTTON_ROOT_STYLE,
+      BUTTON_SIZE_STYLE[size],
       { "rounded-md": rounded },
       { border: variant !== "link" },
       "text-white",
-      { "w-full": fullwidth },
-      classes.colors.variants[variant][color],
-      { [classes.disabled]: disabled },
+      { [BUTTON_FULL_WIDTH_STYLE]: fullwidth },
+      BUTTON_VARIANTS_STYLE[variant][color],
+      { [BUTTON_DISABLED_STYLE]: disabled },
       className
     );
     return (
---
+++ src/consts/button.style.ts
@@ -0,0 +1,73 @@
+export const BUTTON_LINK_STYLE: any = {
+  primary:
+    "px-0 py-0 hover:underline hover:text-blue-800 focus:ring-blue-500 text-blue-600",
+  secondary:
+    "px-0 py-0 hover:underline hover:text-gray-800 focus:ring-gray-500 text-gray-600",
+  danger:
+    "px-0 py-0 hover:underline hover:text-red-800 focus:ring-red-500 text-red-600",
+  orange:
+    "px-0 py-0 hover:underline hover:text-orange-800 focus:ring-orange-500 text-orange-600",
+  neutral:
+    "px-0 py-0 hover:underline hover:text-neutral-800 focus:ring-neutral-500 text-neutral-600",
+  slate:
+    "px-0 py-0 hover:underline hover:text-slate-800 focus:ring-slate-500 text-slate-600",
+  cyan: "px-0 py-0 hover:underline hover:text-cyan-800 focus:ring-cyan-500 text-cyan-600",
+  black:
+    "px-0 py-0 hover:underline hover:text-gray-700 focus:ring-gray-900 text-gray-700",
+};
+
+export const BUTTON_SIZE_STYLE = {
+  sm: "px-2 py-1 text-sm",
+  md: "px-4 py-2",
+  lg: "px-6 py-3 text-md",
+  xl: "px-8 py-4 text-lg",
+};
+
+export const BUTTON_DISABLED_STYLE = "opacity-50 cursor-not-allowed";
+
+export const BUTTON_VARIANT_CONTAINED_STYLE = {
+  primary:
+    "border-blue-600 bg-blue-600 hover:bg-blue-800 focus:ring-blue-600 text-white fill-white",
+  secondary:
+    "border-gray-600 bg-gray-600 hover:bg-gray-800 focus:ring-gray-600 text-white fill-white",
+  danger:
+    "border-red-600 bg-red-600 hover:bg-red-800 focus:ring-red-600 text-white fill-white",
+  orange:
+    "border-orange-600 bg-orange-600 hover:bg-orange-800 focus:ring-orange-600 text-white fill-white",
+  neutral:
+    "border-neutral-600 bg-neutral-600 hover:bg-neutral-800 focus:ring-neutral-600 text-white fill-white",
+  slate:
+    "border-slate-600 bg-slate-600 hover:bg-slate-800 focus:ring-slate-600 text-white fill-white",
+  cyan: "border-cyan-600 bg-cyan-600 hover:bg-cyan-800 focus:ring-cyan-600 text-white fill-white",
+  black:
+    "border-gray-600 bg-gray-900 hover:bg-gray-700 focus:ring-gray-900 text-white fill-white",
+};
+
+export const BUTTON_VARIANT_OUTLINED_STYLE = {
+  primary:
+    "border-blue-600 hover:b-blue-800 focus:ring-blue-500 text-blue-600 fill-blue-500",
+  secondary:
+    "border-gray-600 hover:b-gray-800 focus:ring-gray-500 text-gray-600 fill-gray-500",
+  danger:
+    "border-red-600 hover:b-red-800 focus:ring-red-500 text-red-600 fill-red-500",
+  orange:
+    "border-orange-600 hover:b-orange-800 focus:ring-orange-500 text-orange-600 fill-orange-500",
+  neutral:
+    "border-neutral600 hover:b-neutral-800 focus:ring-neutral-500 text-neutral-600 fill-neutral-500",
+  slate:
+    "border-slate-600 hover:b-slate-800 focus:ring-slate-500 text-slate-600 fill-slate-500",
+  cyan: "border border-cyan-600 hover:b-cyan-800 focus:ring-cyan-500 text-cyan-600 fill-cyan-500",
+  black:
+    "border border-gray-900 hover:b-gray-700 focus:ring-gray-900 text-gray-700 fill-black-500",
+};
+
+export const BUTTON_VARIANTS_STYLE = {
+  contained: BUTTON_VARIANT_CONTAINED_STYLE,
+  outlined: BUTTON_VARIANT_OUTLINED_STYLE,
+  link: BUTTON_LINK_STYLE,
+};
+
+export const BUTTON_FULL_WIDTH_STYLE = "w-full";
+
+export const BUTTON_ROOT_STYLE =
+  "focus:outline-none transition ease-in-out duration-300 font-semibold leading-5 focus:ring focus:ring-opacity-50";
